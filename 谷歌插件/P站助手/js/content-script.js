4ec14c20b57c79196f5cce18f88b00bb967bcaef
luolianfeng <luolianfeng@qq.com>
2020-12-13 22:21:43 +0800

    谷歌插件

---
+++ 谷歌插件/P站助手/js/content-script.js
@@ -0,0 +1,38 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    console.log('我被执行了！');
+});
+(function () {
+    console.log('我被执行了！');
+    eval_scripts = (document.querySelector("#player >script:nth-child(2)") || document.querySelector("#player >script:nth-child(1)"))
+    if (eval_scripts) {
+        raw = eval_scripts.innerHTML
+        raw = `	var playerObjList = {};` + raw
+        t = raw.match("flashvars_[0-9]{1,}")[0]
+        eval(raw)
+        if (t) {
+            play_info = eval(t)
+            var video_info = {}
+            video_info['video_title'] = play_info.video_title
+            video_info['link_url'] = play_info.link_url
+            video_info['cover'] = play_info.image_url
+            video_info['quality'] = []
+
+            Object.keys(play_info).forEach((item) => {
+                console.log(play_info)
+                tmp = []
+                if (item.startsWith('quality_')) {
+
+                    video_info['quality'].push({
+                        quality: item.split("_")[1],
+                        url: play_info[item]
+                    })
+                }
+            })
+            chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+                if (request.cmd == 'init')
+                    sendResponse(video_info);
+            });
+        }
+    }
+
+})()
---
+++ 谷歌插件/P站助手/js/popup.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    sendMessageToContentScript({cmd: 'init', value: 'init'}, function (video_info) {
+        console.log('我被执行了！');
+        console.log(video_info)
+
+        html = '<li><img class="video" src ="' + video_info.cover + '" /><div><h4>' + video_info.video_title + '<h4>'
+
+        for (var i = 0; i < video_info.quality.length; i++) {
+            html += '<a target="_blank" href="' + video_info.quality[i].url + '"><span class="btn"><img class="icon" src="../css/download.svg"/>Download ' + video_info.quality[i].quality + '</span></a>'
+        }
+        html += '</div></li>'
+        $("#video_info").append(html)
+    })
+})()
+
+function append_html(link, cover, video_url) {
+    console.log('append_html->', link, cover, video_url)
+    $("#video_info").append('<li><img class="video" src="' + cover + '"/><div><a target="_blank" href="' + video_url + '"><span class="btn"><img class="icon" src="../css/download.svg"/>Download</span></a><a target="_blank" href="' + "https://tkdownload.vip/?link=" + link + '"><span class="btn"><img class="icon" src="../css/download.svg"/>Download Without WaterMark</span></a></div></li>')
+}
+
+function sendMessageToContentScript(message, callback) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
+            if (callback) callback(response);
+        });
+    });
+}
