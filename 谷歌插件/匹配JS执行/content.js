8fd43be2bfc08a2ad80e1aa994a3dfaa1c213370
luolianfeng <luolianfeng@upchina.com>
2020-12-29 10:27:26 +0800

    匹配JS执行

---
+++ 谷歌插件/匹配JS执行/content.js
@@ -0,0 +1,9 @@
+function init() {
+    console.log('content')
+    document.addEventListener('DOMContentLoaded', function () {
+        alert('百度百度')
+        console.log('hello baidu!')
+    });
+}
+
+init();
---
+++ 谷歌插件/匹配JS执行/popup.js
@@ -0,0 +1,12 @@
+// 获取当前选项卡ID
+function getCurrentTabId(callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (callback) callback(tabs.length ? tabs[0].id : null);
+    });
+}
+
+function init() {
+    
+}
+
+init();
